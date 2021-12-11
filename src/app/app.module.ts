bcf14f319a30b56a7631fc449ab74871ec42fab0
celestin <46505055+celestin98@users.noreply.github.com>
2021-12-11 01:53:02 +0100

    intall ngxs and add action and stade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,21 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import {AngularFireModule} from "@angular/fire/compat";
 import {environment} from "../environments/environment";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+<<<<<<< Updated upstream
+=======
+import { SelectLAnguageComponent } from './components/select-language/select-language.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { AddLanguageComponent } from './components/add-language/add-language.component';
+import { LangueListComponent } from './components/langue-list/langue-list.component';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {ConnectionServiceModule} from 'ng-connection-service';
+import {NgxsLoggerPluginModule} from "@ngxs/logger-plugin";
+import {NgxsModule} from "@ngxs/store";
+import {NgxsReduxDevtoolsPluginModule} from "@ngxs/devtools-plugin";
+
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -23,7 +38,25 @@ import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
+<<<<<<< Updated upstream
     AngularFirestoreModule
+=======
+    NgxsModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    AngularFirestoreModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    HttpClientModule,
+    ConnectionServiceModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
